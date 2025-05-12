d4123b72a5835ee149b613639c1cded086d21ebf
Cameron <cameron.lozano1337@gmail.com>
2025-05-11 22:45:07 -0500

    title: Further development to protectRoute middleware
    body: This commit was focused on further developing the protectRoute middleware. Specifically,
    I need it to reliably, consistently, and accurately verify user credentials in order to
    actually implement the route that I wish to develop next, which is the deleteUser route. Part
    of the development that was done to the protectRoute middleware was focused on catching errors
    that occur whenever the .verify method is called on JWT tokens (to ensure that all JWTs are
    valid). To do this, I wrapped it in promisify, which allowed me to attach a .catch block
    in which I returned an instance of my helper function AppError. This ensures whenever
    an invalid JWT token is sent, a 401 status code is sent back, and access to further activity
    of the protected route is denied.
    
    Immediately afterwards, I wanted to
    ensure that the properties that are actually needed for the rest of the function were present in
    the results of the fulfilled promise (id & iat). I tried to be specific in this
    verification to provide as little room as possible for errors to appear and to reduce the
    possibility of misuse. Only if those properties are present, do I properly destructure them for
    further use in the function. I still need to ensure that the ID obtained from the decoded JWT
    payload is indeed associated with a user in the Users collection of MongoDB.
    This is extremely trivial, will be implemented soon!

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -1,7 +1,10 @@
 // ---------- IMPORTING MODULES
+const { promisify } = require('util');
+
 const JWT = require('jsonwebtoken');
 
 const User = require('../models/userModel');
+const AppError = require('../utility/appError');
 
 // -----------CONFIGURING ENVIRONMENTAL VARIABLES
 require('dotenv').config({ path: '../config/.env' });
@@ -12,21 +15,38 @@ const protectRoute = async (req, res, next) => {
   // 1. VERIFY TOKEN EXISTENCE
   let token;
   if (req?.headers?.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
   }
 
   // 2. VERIFY TOKEN VALIDITY
-  const { id, iat } = JWT.verify(token, process.env.JWT_SECRET);
-  // 3. CHECK IF USER STILL EXIST
+  const promisifyResults = await promisify(JWT.verify)(token, process.env.JWT_SECRET).catch(
+    (err) => {
+      return next(new AppError('Please login again!', 401));
+    }
+  );
+
+  if (!promisifyResults?.id && !promisifyResults?.iat) {
+    return;
+  }
+
+  const { id, iat } = promisifyResults;
+  // 3. CHECK IF USER CHANGED PASSWORDS AFTER THE TOKEN WAS ISSUED
+
+  // 4. CHECK IF USER STILL EXIST
   const user = await User.findById(id);
-  // 4. CHECK IF USER CHANGED PASSWORDS AFTER THE TOKEN WAS ISSUED
+
   // 4-A. A modification is needed to safely check the differences in timestamps between the JWT and the time the password was changed (if at all).
   const fixedTimeStamp = user.passwordChangedAt
     ? parseInt(user.passwordChangedAt.getTime() / 1000, 10)
     : null;
+
   // If the password was changed after (fixedTimeStamp) the issuing of the JWT (iat), then the token is invalid for security reasons.
   if (fixedTimeStamp && fixedTimeStamp > iat) {
-    throw new Error(
-      'Hell nah, you done fucked up A-A-RON IN THE PROTECT ROUTE FUNCTION, PROTECT ROUTE FILE/FOLDER. THIS IS AN ERROR MESSAGE PLACEHOLDER'
+    next(
+      new AppError(
+        'Password was recently changed! Please login again to delete your this account!',
+        401
+      )
     );
   }
   req.user = user;
